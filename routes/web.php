58cbadb7f515612f7976f9bbd8eaefc2e04d8188
IzzatunNauly <naulizzatun159@gmail.com>
2022-02-21 10:39:23 +0700

    Praktikum2(Percobaan2)

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -35,12 +38,26 @@ use App\Http\Controllers\PageController;
 //    echo "Ini adalah halama artikel dengan ID : ".$id; 
 //   });
    
-//PRKTIKUM2
+//PRKTIKUM2(PERCOBAAN1)
 //Nomor1
-Route::get('/', [PageController::class, 'index']); 
+//Route::get('/', [PageController::class, 'index']); 
 
 //Nomor2
-Route::get('/about', [PageController::class, 'about']); 
+//Route::get('/about', [PageController::class, 'about']); 
 
 //Nomor3
-Route::get('/articles/{id}', [PageController::class, 'articles']); 
+//Route::get('/articles/{id}', [PageController::class, 'articles']); 
+
+//PRKTIKUM2(PERCOBAAN2)
+//Nomor1
+//Route::get('/', [HomeController::class, 'index']); 
+//Route::get('/', [AboutController::class, 'index']); 
+Route::get('/', [ArticleController::class, 'index']);
+//Nomor2
+//Route::get('/about', [HomeController::class, 'about']); 
+//Route::get('/about', [AboutController::class, 'about']); 
+Route::get('/about', [ArticleController::class, 'about']); 
+//Nomor3
+//Route::get('/articles/{id}', [HomeController::class, 'articles']); 
+//Route::get('/articles/{id}', [AboutController::class, 'articles']); 
+Route::get('/articles/{id}', [ArticleController::class, 'articles']); 
