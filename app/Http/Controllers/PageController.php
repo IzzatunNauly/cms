d64b7cf15010fa0c53ad8e9097b1e5cb7dea2c5d
IzzatunNauly <naulizzatun159@gmail.com>
2022-02-21 09:59:27 +0700

    Praktikum2(Percobaan1)

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function index(){
+        return 'Hi! Selamat Datang';
+    }
+    public function about(){
+        return 'Izzatun Nauly, 2041720166';
+    }
+    public function articles($id){
+        return 'Ini adalah halaman artikel dengan ID : '.$id;
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\PageController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -19,20 +19,28 @@ use Illuminate\Support\Facades\Route;
 
 //PRAKTIKUM 1
 //Nomor 1
-Route::get('/', function () { 
-    echo "Selamat Datang"; 
-   });
+//Route::get('/', function () { 
+//    echo "Selamat Datang"; 
+//   });
 
 //Nomor2
-Route::get('/about', function () { 
-    echo "NIM : 2041720166 <br>"; 
-    echo "Nama : Izzatun Nauly <br>";
-    echo "Kelas : TI-2H <br>";
-   });
+//Route::get('/about', function () { 
+//    echo "NIM : 2041720166 <br>"; 
+//    echo "Nama : Izzatun Nauly <br>";
+//    echo "Kelas : TI-2H <br>";
+//   });
 
 //Nomor3
-Route::get('/articles/{id}', function ($id) { 
-    echo "Ini adalah halama artikel dengan ID : ".$id; 
-   });
+//Route::get('/articles/{id}', function ($id) { 
+//    echo "Ini adalah halama artikel dengan ID : ".$id; 
+//   });
    
-   
+//PRKTIKUM2
+//Nomor1
+Route::get('/', [PageController::class, 'index']); 
+
+//Nomor2
+Route::get('/about', [PageController::class, 'about']); 
+
+//Nomor3
+Route::get('/articles/{id}', [PageController::class, 'articles']); 
